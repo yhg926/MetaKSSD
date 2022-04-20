ee714ef7259c6efb0dc35846ab052bf0df507840
yhg926 <yhg926@gmail.com>
2022-04-20 17:36:39 +0800

    fix build failure against gcc-10

--- global_wrapper.h
+++ global_wrapper.h
@@ -1,7 +1,7 @@
 #ifndef GLOBAL_RAPER
 #define GLOBAL_RAPER
 
-char *domain;  //assinged in main()
-char *long_domain;   //assinged in main()
+extern char *domain;  //assinged in main()
+extern char *long_domain;   //assinged in main()
 void cmd_global(int argc, char**argv);
 #endif
