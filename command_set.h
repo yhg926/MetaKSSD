c89cada62a704e6483707200c07cb85d4c45bc7e
yhg926 <yhg926@gmail.com>
2024-11-13 08:39:56 +0000

    remove

--- command_set.h
+++
@@ -1,5 +0,0 @@
-#ifndef COMMAND_SET
-#define COMMAND_SET
-	#include <argp.h>
-	int cmd_set(struct argp_state* state);
-#endif 
--- global_wrapper.h
+++
@@ -1,7 +0,0 @@
-#ifndef GLOBAL_RAPER
-#define GLOBAL_RAPER
-
-extern char *domain;  //assinged in main()
-extern char *long_domain;   //assinged in main()
-void cmd_global(int argc, char**argv);
-#endif
