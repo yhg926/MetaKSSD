237a7fc5315fb80f4f41d70b14e6f27a21c359f4
yhg926 <yhg926@gmail.com>
2024-11-14 03:16:37 +0000

    update

--- run_profiling.sh
+++ run_profiling.sh
@@ -7,7 +7,6 @@ SCRIPT_NAME="possion.kssd2out.pl"
 SHUF_F="$PRO_D/shuf_files/L3K11.shuf"
 
 MARKERDB=`dirname $1"/any"` 
-shift
 
 [ -f $KSSD ] || { echo "$KSSD does not exists" ; exit 1 ;}
 [ -f $SCRIPT_D/$SCRIPT_NAME ] || { echo "$SCRIPT_D/$SCRIPT_NAME does not exists" ; exit 1; }
@@ -19,9 +18,9 @@ mkdir -p $TMP_D
 OUT_SKTCH="$TMP_D/Samples_L3K11_sketch"
 RAW_RSLT_F="$TMP_D/species_coverage.tsv"
 OUT_PROFILE_F=$2"_metakssd.profile.tsv"
-
 echo ">> MetaKSSD sektching  ..."
 start_time=$(date +%s)
+shift 
 $KSSD dist -L $SHUF_F -A -o $OUT_SKTCH $@
 [ -d $OUT_SKTCH ] || { echo "aborted!"; exit 1 ;}
 end_time=$(date +%s)
@@ -31,7 +30,7 @@ $KSSD composite -r $MARKERDB -q $OUT_SKTCH > $RAW_RSLT_F
 perl $SCRIPT_D/$SCRIPT_NAME $RAW_RSLT_F $RAW_RSLT_F 18  > $OUT_PROFILE_F
 end_time=$(date +%s)
 echo "... MetaKSSD profile created!: $OUT_PROFILE_F. Elapsed time: $((end_time - start_time)) seconds."
-rm -r $TMP_D
+#rm -r $TMP_D
 
 
 
