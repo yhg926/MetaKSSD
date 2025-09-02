650b0df276be206cde8608d82c2761846137f6e3
Huiguang Yi <yhg926@gmail.com>
2025-09-02 16:01:52 +0800

    Update run_profiling.sh
    cite

--- run_profiling.sh
+++ run_profiling.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+
+citations=$(cat <<'EOF'
+In keeping with academic convention, please cite the works on which your article is based.
+
+If you use MetaKSSD, please cite:
+
+Yi, H., Lu, X., & Chang, Q. (2025). MetaKSSD: boosting the scalability of the reference
+taxonomic marker database and the performance of metagenomic profiling using sketch operations.
+Nature Computational Science. https://doi.org/10.1038/s43588-025-00855-0
+
+If you also refer to the K-mer Substring Space Sampling and Decomposition (KSSD) method, please
+additionally cite:
+
+Yi, H., Lin, Y., Lin, C., et al. (2021). Kssd: sequence dimensionality reduction by k-mer substring
+space sampling enables real-time large-scale datasets analysis. Genome Biology, 22, 84.
+https://doi.org/10.1186/s13059-021-02303-4
+EOF
+)
+
+echo "$citations"
+
 [ -d "$1" ] && [ -f "$2" ] || { echo "USAGE: $0 <MarkerDB> <sample1.fq> ..." ; exit 1; }
 PRO_D=$METAKSSD_PATH
 KSSD="$PRO_D/bin/metakssd"
